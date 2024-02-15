7fce483663af72ed18fc71388f2dd59a25c4b473
mumeinosato <66110797+mumeinosato@users.noreply.github.com>
2024-02-16 02:50:25 +0900

    ヘッダーの準備

---
+++ src/common/Header.vue
@@ -0,0 +1,27 @@
+<template>
+    <header>
+      <nav>
+        <ul class="main-nav">
+          <li><router-link to="/"><img src="../assets/logo.png"></router-link></li>
+          <li><router-link to="/">ホーム</router-link></li>
+          <li><router-link to="/schedule">スケジュール</router-link></li>
+          <li><router-link to="/group">出展</router-link></li>
+          <li><router-link to="/map">マップ</router-link></li>
+          <li><router-link to="/notice">注意事項</router-link></li>
+        </ul>
+      </nav>
+    </header>
+  </template>
+    
+    <script>
+  export default {
+    data() {
+      return {
+        title: "ヘッダー",
+      };
+    },
+  };
+  </script>
+    
+    <style scoped>
+  </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,29 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h2>{{ title }}</h2>
   </main>
 </template>
+    
+<script>
+export default {
+  data() {
+    return {
+      title: "TOPページです。",
+    };
+  },
+};
+</script>
+    
+    <style scoped>
+main {
+  height: calc(100vh - 152px);
+  padding: 3% 0;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+h2 {
+  margin: 0;
+}
+</style>
